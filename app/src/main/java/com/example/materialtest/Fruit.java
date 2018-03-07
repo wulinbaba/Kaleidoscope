c9621d219b0c7bd1d7d7676798969913004a25ec
wulinbaba <598132064@qq.com>
2018-03-07 16:30:18 +0800

    Interface Design
    Interface Design with Material Design

---
+++ app/src/main/java/com/example/materialtest/Fruit.java
@@ -0,0 +1,24 @@
+package com.example.materialtest;
+
+/**
+ * Created by wulin on 2018/3/1.
+ */
+
+public class Fruit {
+    private  String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getImageId(){
+        return imageId;
+    }
+}
